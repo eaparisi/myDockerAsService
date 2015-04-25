ee883eb2174f62b152d4e33d5dadb4e06568424d
eric@yeswead.com <eric@yeswead.com>
2015-04-24 23:04:07 -0300

    -o-o-o

---
+++ Dockerfile_Back/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cp /root/pw.txt /root/pw_old.txt
+echo $ROOTPASS > /root/pw.txt
+echo "root:$(cat /root/pw.txt)" | chpasswd
+
+a2ensite myvhost_frontend.conf
+a2ensite myvhost_backend.conf
+a2ensite myvhost_static.conf
+
+php5enmod mcrypt
+
+service apache2 restart
+
+cp /usr/share/zoneinfo/America/Buenos_Aires /etc/localtime
+
+/usr/sbin/sshd -D &
+wait ${!}
